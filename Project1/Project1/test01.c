73658e6bcc831f2d34a13602f0fed31126a53b1d
junsoowoo <hanjs991124@gmail.com>
2024-04-12 18:11:53 +0900

    오목test1 오류 고침

--- Project1/Project1/test01.c
+++ Project1/Project1/test01.c
@@ -1,16 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
 void put_black();
 void put_white();
 void draw_board();
-char board[19][19] ;
-int black_cnt = 0;
-int white_cnt = 0;
+//초기값 설정
+char board[19][19];
+int b_cnt = 0;
+int w_cnt = 0;
 
 int main()
 {
-	//바둑판 생성
+	//초기 보드판 그리기
 	for (int i = 0; i < 19; i++)
 	{
 		for (int j = 0; j < 19; j++)
@@ -20,7 +20,6 @@ int main()
 		}
 		printf("\n");
 	}
-	printf("=====================================\n");
 	//돌 놓는 사이클
 	while (1)
 	{
@@ -29,54 +28,7 @@ int main()
 	}
 	return 0;
 }
-
-// 검은돌 놓기
-void put_black()
-{
-	int x, y;
-	printf("검정돌이 놓일 좌표를 입력하세요.(0~18)\n");
-	scanf("%d%d", &x, &y);
-	if (board[x][y] != '+')
-	{
-		printf("빈자리가 아닙니다. 다시 입력하세요.\n");
-		scanf("%d%d", &x, &y);
-	}
-	if (x < 0 || x > 18 || y < 0 || y > 18)
-	{
-		printf("좌표 범위를 벗어났습니다. 다시 입력하세요.\n");
-		scanf("%d%d", &x, &y);	
-	}
-	else
-		board[x][y] = 'B';
-	draw_board();
-	printf("검정돌 개수: %d\n", ++black_cnt);
-	printf("흰돌 개수: %d\n", white_cnt);
-}
-
-// 흰돌 놓기
-void put_white()
-{
-	int x, y;
-	printf("흰돌이 놓일 좌표를 입력하세요.(0~18)\n");
-	scanf("%d%d", &x, &y);
-	if (board[x][y] != '+')
-	{
-		printf("빈자리가 아닙니다. 다시 입력하세요.\n");
-		scanf("%d%d", &x, &y);
-	}
-	if (x < 0 || x > 18 || y < 0 || y > 18)
-	{
-		printf("좌표 범위를 벗어났습니다. 다시 입력하세요.\n");
-		scanf("%d%d", &x, &y);
-	}
-	else
-		board[x][y] = 'W';
-	draw_board();
-	printf("검정돌 개수: %d\n", black_cnt);
-	printf("흰돌 개수: %d\n", ++white_cnt);
-}
-
-//현 바둑판 상황 그리기
+//현 보드 상황 그리는 함수
 void draw_board()
 {
 	for (int i = 0; i < 19; i++)
@@ -87,4 +39,44 @@ void draw_board()
 		}
 		printf("\n");
 	}
+}
+//검은 돌 놓는 함수
+void put_black()
+{
+	int x, y;
+
+	printf("검은돌이 놓일 좌표를 입력하세요.(0~18).\n");
+	scanf("%d%d", &x, &y);
+	if (x < 0 || x >= 19 || y < 0 || y >= 19 || board[x][y] != '+')
+	{
+		printf("잘못된 입력입니다. 다시 입력하세요.\n");
+		put_black(); 
+		return;
+	}
+	b_cnt++;
+	board[x][y] = 'B';
+	draw_board();
+
+	printf("검은돌의 개수:%d\n", b_cnt);
+	printf("흰돌의 개수:%d\n", w_cnt);
+}
+//흰돌 놓는 함수
+void put_white()
+{
+	int x, y;
+
+	printf("흰돌이 놓일 좌표를 입력하세요.(0~18).\n");
+	scanf("%d%d", &x, &y);
+	if (x < 0 || x >= 19 || y < 0 || y >= 19 || board[x][y] != '+')
+	{
+		printf("잘못된 입력입니다. 다시 입력하세요.\n");
+		put_white(); 
+		return;
+	}
+	w_cnt++;
+	board[x][y] = 'W';
+	draw_board();
+
+	printf("검은돌의 개수:%d\n", b_cnt);
+	printf("흰돌의 개수:%d\n", w_cnt);
 }
